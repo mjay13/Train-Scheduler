a5e83da2de3aab3512b7a8fbd3f6614a617f0620
Megan Jacobs <mjay815@hotmail.com>
2017-09-09 20:17:34 -0400

    general cleanup of notes and such

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,22 +10,10 @@ var config = {
 
 firebase.initializeApp(config);
 
-// firebase var
+$(document).ready(function() {
+
+// firebase variable
 var database = firebase.database();
-
-//global variables for train input values //*******change to put the variables inside each of the functions
-
-// ********* they don't need to be global as the use of the database can grab all of the information essentially out of the on click function
-// var inputTrainName;
-// var inputDestination;
-// var inputFirstArrival; //////
-// var firstArrival = moment(inputFirstArrival, "HH:mm").subtract(1, "years"); // time the train starts running
-// var inputFrequency; 
-// var currentTime = moment(); // current time
-// var totalDiff = moment().diff(moment.unix(firstArrival), "minutes"); // difference between the when the train starts running and current time
-// var minuteMod = totalDiff % inputFrequency; // modulous for the totalDiff mod. the frequency of the trains
-// var minutesAway = inputFrequency - minuteMod; // how many minutes away the next train is
-// var nextArrival = moment().add(minutesAway, "m").format("HH:mm"); // time in military of next train arriving
 
 
 // on click for submitting the form to firebase
@@ -44,17 +32,11 @@ $("#submit").on("click", function() {
         destination: inputDestination,
         firstArrival: inputFirstArrival,
         frequency: inputFrequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP //giving me an error that TIMESTAMP is not defined -- fixed database here is not the same as the one i created! this is a standard for firebase that cannot be messed with
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
     };
 
     // push "object" to database child "trains"
-    database.ref("trains").push(trainObject); //why can't i push this to a child/create a child in my database? why can i only push to the root? -- it needs to be in string form!
-    // inputTrainName: inputTrainName,
-    // inputDestination: inputDestination,
-    // inputFirstArrival: inputFirstArrival,
-    // inputFrequency: inputFrequency,
-    // dateAdded: firebase.database.ServerValue.TIMESTAMP //giving me an error that TIMESTAMP is not defined -- fixed database here is not the same as the one i created! this is a standard for firebase that cannot be messed with
-
+    database.ref("trains").push(trainObject); 
     alert("train added!");
 
     console.log(name);
@@ -73,37 +55,29 @@ $("#submit").on("click", function() {
 
 // diplay/add to html
 database.ref("trains").on("child_added", function(snapshot) {
-        // create new variables to deal with the new information coming from the database. 
-        var nameData = snapshot.val().name;
-        var destinationData = snapshot.val().destination;
-        var firstArrivalData = snapshot.val().inputFirstArrival;
-        var frequencyData = snapshot.val().frequency;
-
         // current time
-         //var currentTime = moment(); // current time
+        currentTime = moment().format("HH:mm");
 
-        // first arrival from train data
-        //var firstArrival = moment(firstArrivalData, "HH:mm").subtract(1, "years"); // time the train starts running
+        // create new variables to deal with the new information coming from the database. 
+        var destinationData = snapshot.val().inputDestination;
+        var firstArrivalData = snapshot.val().inputFirstArrival;
+        var frequencyData = snapshot.val().inputFrequency;
+
         
         // difference from the first arrival to current time
-        var totalDiff = moment().diff(moment.unix(firstArrivalData), "minutes"); // difference between the when the train starts running and current time
+        var totalDiff = moment().diff(moment.unix(firstArrivalData), "minutes");
         
         // modulous from the total difference in minutes and the frequency at which the train runs
-        var minuteMod = totalDiff % inputFrequency;
+        var minuteMod = totalDiff % frequencyData;
         
         // the next train in minutes is the frequency of minutes that the train runs minus the modulous
-        var minutesAway = inputFrequency - minuteMod; 
+        var minutesAway = frequencyData - minuteMod; 
 
         // in military time showing when the next train arrives
         var nextArrival = moment().add(minutesAway, "m").format("HH:mm");
 
 
-
-
-
-
-
-        // *********** change 
+        // adds to html
         $("#trainInfo").append("<tr><td>" + snapshot.val().inputTrainName + "</td>" +
             "<td>" + snapshot.val().inputDestination + "</td>" +
             "<td>" + snapshot.val().inputFrequency + "</td>" +
@@ -113,8 +87,10 @@ database.ref("trains").on("child_added", function(snapshot) {
         // show train object data
         console.log(snapshot.val());
 
+        
+        // log information
+        console.log("Time: " + currentTime);
         console.log("Start Time: " + firstArrivalData);
-        //console.log("Current Time: " + moment(currentTime).format("HH:mm"));
         console.log("Total Time Difference: " + totalDiff);
         console.log("Modulous: " + minuteMod);
         console.log("Minutes Until Next Train: " + minutesAway);
@@ -124,9 +100,4 @@ database.ref("trains").on("child_added", function(snapshot) {
         console.log("The read failed: " + errorObject.code);
     });
 
-
-// convert start time to minutes/give a date? input firstArrival = startTime
-// convert current time to minutes "time"
-// time - startTime = inBetweenTime
-// inBetweenTime modulous % inputFrequency = minuteMod
-// inputFrequency - minuteMod = minutesAway
+});
