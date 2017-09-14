58056f58043489488ea2320d7da1fba9f6f2107e
Megan Jacobs <mjay815@hotmail.com>
2017-09-14 16:47:19 -0400

    more work on js, unsuccessful so far

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,99 +4,102 @@ var config = {
     authDomain: "train-time-a753e.firebaseapp.com",
     databaseURL: "https://train-time-a753e.firebaseio.com",
     projectId: "train-time-a753e",
-    storageBucket: "",
+    storageBucket: "train-time-a753e.appspot.com",
     messagingSenderId: "197917481665"
-};
+  };
 
-firebase.initializeApp(config);
+  firebase.initializeApp(config);
 
-$(document).ready(function() {
 
-    // firebase variable
-    var database = firebase.database();
 
-    // on click for submitting the form to firebase
-    $("#submit").on("click", function() {
-        event.preventDefault();
+// firebase variable
+var database = firebase.database();
 
-        // set variables to the submitted information
-        var inputTrainName = $("#inputTrainName").val().trim();
-        var inputDestination = $("#inputDestination").val().trim();
-        var inputFirstArrival = $("#inputFirstArrival").val().trim();
-        var inputFrequency = $("#inputFrequency").val().trim();
+// on click for submitting the form to firebase
+$("#submit").on("click", function() {
+    event.preventDefault();
 
-        // variable to creating a new object with the individual train information stored inside
-        var trainObject = {
-            name: inputTrainName,
-            destination: inputDestination,
-            firstArrival: inputFirstArrival,
-            frequency: inputFrequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
-        };
+    // set variables to the submitted information
+    var inputTrainName = $("#inputTrainName").val().trim();
+    var inputDestination = $("#inputDestination").val().trim();
+    var inputFirstArrival = moment($("#inputFirstArrival").val().trim(), "HH:mm").subtract(1, "years").format("X");
+    var inputFrequency = $("#inputFrequency").val().trim();
 
-        // push "object" to database child "trains"
-        database.ref("trains").push(trainObject);
+    // variable to creating a new object with the individual train information stored inside
+    var trainObject = {
+        name: inputTrainName,
+        destination: inputDestination,
+        firstArrival: inputFirstArrival,
+        frequency: inputFrequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
 
-        // add a modal instead of an alert?
-        //alert("train added!");
+    // push "object" to database child "trains"
+    database.ref("trains").push(trainObject);
 
-        // clear form
-        $("#inputTrainName").val("");
-        $("#inputDestination").val("");
-        $("#inputFirstArrival").val("");
-        $("#inputFrequency").val("");
+    // add a modal instead of an alert?
+    alert("train added!");
 
-        return false;
+    // clear form
+    $("#inputTrainName").val("");
+    $("#inputDestination").val("");
+    $("#inputFirstArrival").val("");
+    $("#inputFrequency").val("");
+
+    return false;
+
+    // log train info
+    console.log(trainObject.name);
+    console.log(trainObject.destination);
+    console.log(trainObject.firstArrival);
+    console.log(trainObject.frequency);
 
     // end of form submission    
-    });
+});
 
-    // diplay/add to html
-    database.ref("trains").on("child_added", function(snapshot) {
-            // current time
-            currentTime = moment().format("HH:mm");
+// diplay/add to html
+database.ref().on("child_added", function(snapshot) {
+        // current time
+        currentTime = moment().format("HH:mm");
 
-            // create new variables to deal with the new information coming from the database. 
-            var destinationData = snapshot.val().inputDestination;
-            var firstArrivalData = snapshot.val().inputFirstArrival;
-            var frequencyData = snapshot.val().inputFrequency;
+        // create new variables to deal with the new information coming from the database. 
+        var destinationData = snapshot.val().destination;
+        var firstArrivalData = snapshot.val().firstArrival;
+        var frequencyData = snapshot.val().frequency;
 
-            // difference from the first arrival to current time
-            var totalDiff = moment().diff(moment.unix(firstArrivalData), "minutes");
+        // difference from the first arrival to current time
+        var totalDiff = moment().diff(moment.unix(firstArrivalData), "minutes");
 
-            // modulous from the total difference in minutes and the frequency at which the train runs
-            var minuteMod = totalDiff % frequencyData;
+        // modulous from the total difference in minutes and the frequency at which the train runs
+        var minuteMod = totalDiff % frequencyData;
 
-            // the next train in minutes is the frequency of minutes that the train runs minus the modulous
-            var minutesAway = frequencyData - minuteMod;
+        // the next train in minutes is the frequency of minutes that the train runs minus the modulous
+        var minutesAway = frequencyData - minuteMod;
 
-            // in military time showing when the next train arrives
-            var nextArrival = moment().add(minutesAway, "m").format("HH:mm");
+        // in military time showing when the next train arrives
+        var nextArrival = moment().add(minutesAway, "m").format("HH:mm");
 
 
-            // adds to html
-            $("#trainInfo").append("<tr><td>" + snapshot.val().inputTrainName + "</td>" +
-                "<td>" + snapshot.val().inputDestination + "</td>" +
-                "<td>" + snapshot.val().inputFrequency + "</td>" +
-                "<td>" + snapshot.val().nextArrival + "</td>" +
-                "<td>" + snapshot.val().minutesAway + "</td></tr>");
+        // adds to html
+        $("#trainInfo").append("<tr><td>" + snapshot.val().name + "</td>" +
+            "<td>" + snapshot.val().destination + "</td>" +
+            "<td>" + snapshot.val().frequency + "</td>" +
+            "<td>" + snapshot.val().nextArrival + "</td>" +
+            "<td>" + snapshot.val().minutesAway + "</td></tr>");
 
-            // show train object data
-            console.log(snapshot.val());
+        // show train object data
+        console.log(snapshot.val());
 
-            // log information
-            console.log("Time: " + currentTime);
-            console.log("Start Time: " + firstArrivalData);
-            console.log("Total Time Difference: " + totalDiff);
-            console.log("Modulous: " + minuteMod);
-            console.log("Minutes Until Next Train: " + minutesAway);
-            console.log("Next Train Arrives: " + nextArrival);
-        },
-        function(errorObject) {
-            console.log("The read failed: " + errorObject.code);
+        // log information
+        console.log("Time: " + currentTime);
+        console.log("Start Time: " + firstArrivalData);
+        console.log("Total Time Difference: " + totalDiff);
+        console.log("Modulous: " + minuteMod);
+        console.log("Minutes Until Next Train: " + minutesAway);
+        console.log("Next Train Arrives: " + nextArrival);
+    },
+    function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
 
         // end of database referencing
-        });
-
-// end of document ready
-});
+    });
